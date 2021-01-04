b9e5a2d0554a0e9f92c6adcc7e1497ce1324b239
rezaavoor <reza.avoor@gmail.com>
2021-01-04 13:29:13 +0100

    add theme

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,13 @@
 import Head from 'next/head'
-import { css } from '@emotion/react'
+import { css, ThemeProvider } from '@emotion/react'
 import React from 'react'
 import Footer from './Footer'
 import Navbar from './Navbar'
+import theme from '../styles/theme'
 
 export default function Layout(props) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
         <title>Shopium</title>
         <link rel="icon" href="/Icon.ico" />
@@ -14,12 +15,13 @@ export default function Layout(props) {
       <div
         css={css`
           background-color: pink;
+          align-items: center;
         `}
       >
         <Navbar />
-        {props.children}
+        <div>{props.children}</div>
         <Footer />
       </div>
-    </>
+    </ThemeProvider>
   )
 }
