88cc4cae80ebd6c98c92fd5346aae47e4169304c
rezaavoor <reza.avoor@gmail.com>
2021-01-04 13:27:56 +0100

    add default colors and sizes

---
+++ styles/theme.js
@@ -0,0 +1,18 @@
+export default {
+  colors: {
+    primary: '#DEE8F1',
+    secondary: '#EDF3F3',
+    darkblue: '#0E3665',
+    gray: '#707070',
+    white: '#FFFFFF',
+    b_primary: '#F04C5A', // blocket colors
+    b_secondary: '#EFD8DA',
+    t_primary: '#FFCF3C', // tradera colors
+    t_secondary: '#FFF8E4',
+    s_primary: '#15A957', // shpock colors
+    s_secondary: '#F1FFF7',
+  },
+  breakpoints: {
+    //TODO: add proper breakpoints
+  },
+}
