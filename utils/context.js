84d37e90aca8958d0d9c47fdfff42005a3af1b79
rezaavoor <reza.avoor@gmail.com>
2021-01-06 12:23:14 +0100

    add context

---
+++ utils/context.js
@@ -0,0 +1,20 @@
+import React, { useState, createContext } from 'react'
+
+export const Context = createContext()
+
+export const ContextProvider = (props) => {
+  const [searchWord, setSearchWord] = useState('LG tv')
+  const [page, setPage] = useState({
+    page: 1,
+    od: '',
+  })
+
+  const contextData = {
+    searchWord: [searchWord, setSearchWord],
+    page: [page, setPage],
+  }
+
+  return (
+    <Context.Provider value={contextData}>{props.children}</Context.Provider>
+  )
+}
