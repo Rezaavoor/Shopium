8fcb7983bda0735d5b09071b1c0c4b4a0fba8d03
rezaavoor <reza.avoor@gmail.com>
2021-01-07 12:51:39 +0100

    add footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,30 @@
+import { css, useTheme } from '@emotion/react'
 import React from 'react'
 
 export default function Footer() {
-  return <div>Footer</div>
+  const theme = useTheme()
+  return (
+    <div
+      css={css`
+        background-color: ${theme.colors.primary};
+        height: 300px;
+        position: relative;
+      `}
+    >
+      <p
+        css={css`
+          position: absolute;
+          font-weight: 700;
+          bottom: 0;
+          left: 50%;
+          transform: translateX(-50%);
+        `}
+      >
+        {'2021 '}
+        <a href="https://www.reza.social" target="_blank">
+          Reza Hosseini
+        </a>
+      </p>
+    </div>
+  )
 }
