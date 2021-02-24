31d8ab85f7d4df79dcb17220e60296585e221d89
Reza Hosseini <Reza.avoor@gmail.com>
2021-02-24 17:05:43 +0000

    fix typo

--- pages/saved.js
+++ pages/saved.js
@@ -90,7 +90,7 @@ export default function Saved({ savedItems }) {
             text-align: center;
           `}
         >
-          <h4>{'Du har inga sparade annonser'}</h4>
+          <h4>{'Inga annonser hittades'}</h4>
         </div>
       )}
     </div>
