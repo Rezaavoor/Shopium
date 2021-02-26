0ba4c447942d328f991d40a2766cf4ca5e0d3182
Reza Hosseini <Reza.avoor@gmail.com>
2021-02-26 13:10:01 +0000

    fix token generator route not resolving

--- pages/api/getBearerToken.js
+++ pages/api/getBearerToken.js
@@ -1,14 +1,13 @@
 import axios from 'axios'
 
 export default async (req, res) => {
-  axios.get('https://www.blocket.se/om/press').then((response) => {
-    const html = response.data
-    const firstWord = '"authentication":{"loggedIn":false,"bearerToken":"'
-    const lastWord = '","apiKey":'
-    const firstIndex = html.search(firstWord) + firstWord.length
-    const lastIndex = html.search(lastWord)
-    const token = html.substring(firstIndex, lastIndex)
-    res.statusCode = 200
-    res.json({ token })
-  })
+  const response = await axios.get('https://www.blocket.se/om/press')
+  const html = response.data
+  const firstWord = '"authentication":{"loggedIn":false,"bearerToken":"'
+  const lastWord = '","apiKey":'
+  const firstIndex = html.search(firstWord) + firstWord.length
+  const lastIndex = html.search(lastWord)
+  const token = html.substring(firstIndex, lastIndex)
+  res.statusCode = 200
+  res.json({ token })
 }
